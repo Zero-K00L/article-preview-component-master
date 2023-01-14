1f7ff65f91842566dbaeaa39f930cf78d696ee9a
John Grudzinski <johngrudzinski@yahoo.com>
2023-01-14 15:43:07 -0500

    Mobile-Complete

---
+++ script.js
@@ -0,0 +1,14 @@
+const shareBtn = document.querySelector('.share-icon');
+const shareBtnActive = document.querySelector('.share-icon-active');
+var articleInfo = document.querySelector('.article-info');
+var shareMenu = document.querySelector('.share-menu');
+
+shareBtn.addEventListener('click', function() {   //swaps out article info for share menu
+    articleInfo.classList.add('hidden');
+    shareMenu.classList.remove('hidden');
+});
+
+shareBtnActive.addEventListener('click', function() {
+    shareMenu.classList.add('hidden');
+    articleInfo.classList.remove('hidden');
+});
