7a252ba57d1231075cadcc2b5d62fdd2eaeaa016
John Grudzinski <johngrudzinski@yahoo.com>
2023-01-15 14:34:16 -0500

    Desktop-menu-layout-complete

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const shareBtn = document.querySelector('.icon-container');
 const shareBtnActive = document.querySelector('.icon-container-active');
 var articleInfo = document.querySelector('.article-info');
 var shareMenu = document.querySelector('.share-menu');
+var triangle = document.querySelector('.triangle');
 
 shareBtn.addEventListener('click', function() {   //swaps out article info for share menu
     articleInfo.classList.add('hidden');
     shareMenu.classList.remove('hidden');
+    triangle.classList.remove('opacity');
 });
 
 shareBtnActive.addEventListener('click', function() {
     shareMenu.classList.add('hidden');
+    triangle.classList.add('opacity');
     articleInfo.classList.remove('hidden');
 });
